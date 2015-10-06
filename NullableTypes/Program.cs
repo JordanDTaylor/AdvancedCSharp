c0bbf3e69942fbcfd2d7ef15ba67622235d6ad56
Jordan Taylor <jordandtaylor@gmail.com>
2015-10-06 01:22:43 -0600

    Added NullableTypes

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = DateTime.Parse(args[0]);
+
+            //null coalescing operator--
+            DateTime date2 = date ?? DateTime.UtcNow;
+
+            Console.WriteLine(date2);
+        }
+    }
+}
